96ed07a6a8d877785d945a4aad28caa4ecc00a16
PierluigiFrascogna <frascogna.pierluigi@gmail.com>
2025-05-29 00:31:15 +0200

    added tag for docker image

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-docker build ${1:+--build-arg objToCompile="$1"} -t lasdimg .
+docker build ${1:+--build-arg objToCompile="$1"} -t lasdimg:latest .
 
 docker run \
     -it \
     --name LASDdev \
     --rm \
-    lasdimg
+    lasdimg:latest
